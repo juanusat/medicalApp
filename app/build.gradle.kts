22cf626bfaad6231c985525b51851b1991e8a0bc
juanusat <71309030@usat.pe>
2026-04-09 16:39:38 -0500

    mobile navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    // Navegacion
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
