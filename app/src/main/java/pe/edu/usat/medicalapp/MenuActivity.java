37228906102c8ca05d7c1271a93fc2122c8d0738
juanusat <71309030@usat.pe>
2026-04-09 18:26:45 -0500

    vincular menu lateral con fragments

--- app/src/main/java/pe/edu/usat/medicalapp/MenuActivity.java
+++ app/src/main/java/pe/edu/usat/medicalapp/MenuActivity.java
@@ -7,18 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.ui.AppBarConfiguration;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        /*setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
     }
 }
