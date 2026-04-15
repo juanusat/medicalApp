b6c1eab185df08db68320a0fd4f655e21580fb26
juanusat <71309030@usat.pe>
2026-04-15 15:44:55 -0500

    fragment

--- app/src/main/java/pe/edu/usat/medicalapp/InicioFragment.java
+++ app/src/main/java/pe/edu/usat/medicalapp/InicioFragment.java
@@ -2,14 +2,18 @@ package pe.edu.usat.medicalapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class InicioFragment extends Fragment {
+import pe.edu.usat.medicalapp.databinding.FragmentInicioBinding;
 
+public class InicioFragment extends Fragment {
+    FragmentInicioBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,10 @@ public class InicioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentInicioBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle )
 }
